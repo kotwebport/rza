bb4d159f752358886d27c4645c4b49060d63fa9e
kotwebport <kotwebport@gmail.com>
2019-07-01 15:23:33 +0300

    Изменил загрузку разделов, теперь через ajax

--- views/rza/_section.php
+++ views/rza/_section.php
@@ -6,13 +6,13 @@ use yii\bootstrap\NavBar;
 ?>
 <a href="<?= Url::toRoute(['rza/index'])?>">К разделам <a/>
 <div class="">
-    <?php foreach ($menu->tests as $test): ?>
-        <li><a class="js-test" href="<?= Url::toRoute(['rza/test', 'testId' => $test['id']]); ?>"
+    <?php foreach ($tests as $test): ?>
+        <li><a class="js-getAjax" href="<?= Url::toRoute(['rza/test', 'testId' =>3]); ?>"
 			   data-url="rza/test" data-id="<?= $test['id'] ?>"><?= $test['name'] ?></a></li>
     <?php endForeach; ?>
 </div>
-<h1><?= $menu->section->name ?></h1>
-<div class="js_content">
+<h1><?= $section['name'] ?></h1>
+<div class="js-test">
 
 </div>
    
--- views/rza/index.php
+++ views/rza/index.php
@@ -1,1 +1,5 @@
 <h1 style="margin-top: 200px;" >РЗА</h1>
+
+<div class="js-section" >
+	
+</div>
